7017bad9e4080e1597a8a591a93b2cb49504ccdc
Tim Frazier <tim.frazier@lacework.net>
2021-07-23 13:14:37 -0400

    Adding EKS IAM role

--- aws/base/surfer_store/alb_controller.tf
+++ aws/base/surfer_store/alb_controller.tf
@@ -12,6 +12,30 @@ provider "kubernetes" {
   load_config_file       = false
 }
 
+resource "aws_iam_role" "example" {
+  name = "eks-cluster-example"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.example.name
+}
+
 module "alb-ingress-controller" {
   source  = "iplabs/alb-ingress-controller/kubernetes"
   version = "3.4.0"
